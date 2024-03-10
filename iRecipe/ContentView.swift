b6021cae7a15c8f8a79db8321edf6eb5223207a2
feihuan1 <ericrealtor1997@gmail.com>
2024-03-09 16:36:24 -0800

    Initial Commit

---
+++ iRecipe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iRecipe
+//
+//  Created by Feihuan Peng on 3/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iRecipe/iRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  iRecipeApp.swift
+//  iRecipe
+//
+//  Created by Feihuan Peng on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
