42ba90250dfe29980df28c7687b9f5ba6c7258be
feihuan1 <ericrealtor1997@gmail.com>
2024-03-12 10:42:45 -0700

    add category and ingredients

---
+++ iRecipe/Views/IngredientsCell.swift
@@ -0,0 +1,18 @@
+//
+//  IngredientsCell.swift
+//  iRecipe
+//
+//  Created by Feihuan Peng on 3/11/24.
+//
+
+import SwiftUI
+
+struct IngredientsCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IngredientsCell()
+}
